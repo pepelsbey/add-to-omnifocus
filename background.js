899175b4a936da4c61ff951159fd2d00dea2ef20
Vadim Makeev <pepelsbey@gmail.com>
2015-12-04 00:42:02 +0100

    Code style

--- background.js
+++ background.js
@@ -1,20 +1,21 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.executeScript(tab.id, {
-        // run script to try to get user selected text
-        code: "window.getSelection().toString()"
-    },function(selection) {
-        // format selection if it is not null
-        selection = selection[0] ? "\n" + selection[0]: "";
+        // Run script to try to get user selected text
+        code: 'window.getSelection().toString()'
+    }, function(selection) {
+        // Format selection if it’s not null
+        selection = selection[0] ? '\n\n' + selection[0] : '';
 
         chrome.tabs.update(tab.id, {
             url: 'omnifocus://'
         },function() {
             setTimeout(function() {
                 chrome.tabs.update(tab.id, {
-                    url: 'omnifocus:///add?name=' + encodeURIComponent(tab.title) + '&note=' + encodeURIComponent(tab.url+selection)
+                    url:
+                        'omnifocus:///add?name=' + encodeURIComponent(tab.title) +
+                        '&note=' + encodeURIComponent(tab.url + selection)
                 });
-            },300);
+            }, 300);
         });
     });
-
 });
